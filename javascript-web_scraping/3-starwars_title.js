777316e736b98e0626a6599931590a532bfaed0e
nalule-grace <g.nalule@alustudent.com>
2023-06-14 01:25:39 +0000

    js web scraping

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const request = require("request");
-let url =  "https://swapi-api.alx-tools.com/api/films/:id" + process.argv[2];
+const request = require('request');
+let url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
 request(url, function (error, response, body) {
   console.log(JSON.parse(body).title);
 });
+
